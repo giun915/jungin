5b2247ac79716c627c7c957c125ae2f7be534bc7
DESKTOP-61V2IID\gigiu <giun915@gmail.com>
2026-03-09 13:45:36 +0900

    refactor: move global layout components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import {RouterView} from "vue-router";
+import AppHeader from "./components/layout/AppHeader.vue";
+import AppFooter from "./components/layout/AppFooter.vue";
+import BackToTopButton from "./components/common/BackToTopButton.vue";
 </script>
 
 <template>
+  <AppHeader />
   <RouterView />
+  <AppFooter />
+  <BackToTopButton />
 </template>
 
 <style scoped>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
-import AppHeader from '@/components/layout/AppHeader.vue'
 import HeroBanner from '@/components/hero/HeroBanner.vue';
 import { heroSlides } from '@/constants/hero/heroSlides'
 import AboutSection from '@/components/main/AboutSection.vue';
 import PortfolioSection from '@/components/main/PortfolioSection.vue';
-import AppFooter from '@/components/layout/AppFooter.vue';
-import BackToTopButton from '@/components/common/BackToTopButton.vue';
 </script>
 
 <template>
-    <AppHeader />
     <main>
         <HeroBanner :slides="heroSlides"/>
         <AboutSection/>
         <PortfolioSection/>
     </main>
-    <AppFooter />
-    <BackToTopButton />
 </template>
 
 <style scoped>
