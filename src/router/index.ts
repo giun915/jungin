589abcb2b6d9307437771f590ef34e26cfe6938b
DESKTOP-61V2IID\gigiu <giun915@gmail.com>
2026-03-15 09:13:42 +0900

    fix: update asset paths for github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,7 +18,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to) {
     if (to.hash) return { el: to.hash, behavior: 'smooth' }
