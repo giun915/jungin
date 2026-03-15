7fcf495f04fd4e1f6ab2e96f45cf2741fe95e5e6
DESKTOP-61V2IID\gigiu <giun915@gmail.com>
2026-03-15 09:18:04 +0900

    fix: update asset paths for github pages

--- src/constants/layout/headerData.ts
+++ src/constants/layout/headerData.ts
@@ -15,19 +15,19 @@ export const snsLinks: SnsLink[] = [
     {
     id: 'kakao',
     href: 'https://open.kakao.com/o/sJl2Bn4e',
-    icon: '/imgs/icons/kakao.png',
+    icon: `${import.meta.env.BASE_URL}imgs/icons/kakao.png`,
     alt: '카카오톡 로고',
   },
   {
     id: 'insta',
     href: 'https://www.instagram.com/yigiun?igsh=MTVqb3ppeGFxcGRoMA==',
-    icon: '/imgs/icons/insta.png',
+    icon: `${import.meta.env.BASE_URL}imgs/icons/insta.png`,
     alt: '인스타그램 로고',
   },
   {
     id: 'blog',
     href: '#',
-    icon: '/imgs/icons/blog.jpg',
+    icon: `${import.meta.env.BASE_URL}imgs/icons/blog.jpg`,
     alt: '블로그 로고',
   },
 ]
